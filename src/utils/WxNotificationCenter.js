84ebfc2934b1ae66bb5e5b7b363def583502cbbe
zzzzgc <1098766713@qq.com>
2018-07-18 20:05:50 +0800

    工具类目录结构变更

--- src/utils/WxNotificationCenter.js
+++ src/utils/WxNotificationCenter.js
@@ -9,8 +9,9 @@
  * detail : http://weappdev.com/t/wxnotificationcenter/233
  */
 // 存放
-var __notices = [];
-var isDebug = true;
+var __notices = []
+var isDebug = true
+
 /**
  * addNotification
  * 注册通知对象方法
@@ -21,22 +22,22 @@ var isDebug = true;
  * observer: 注册对象，指Page对象
  */
 function addNotification(name, selector, observer) {
-    if (name && selector) {
-        if(!observer){
-            console.log("addNotification Warning: no observer will can't remove notice");
-        }
-        console.log("addNotification:" + name);
-        var newNotice = {
-            name: name,
-            selector: selector,
-            observer: observer
-        };
-
-        addNotices(newNotice);
-
-    } else {
-        console.log("addNotification error: no selector or name");
+  if (name && selector) {
+    if (!observer) {
+      console.log('addNotification Warning: no observer will can\'t remove notice')
     }
+    console.log('addNotification:' + name)
+    var newNotice = {
+      name: name,
+      selector: selector,
+      observer: observer
+    }
+
+    addNotices(newNotice)
+
+  } else {
+    console.log('addNotification error: no selector or name')
+  }
 }
 
 /**
@@ -48,39 +49,39 @@ function addNotification(name, selector, observer) {
  * observer: 注册对象，指Page对象
  */
 function addOnceNotification(name, selector, observer) {
-    if (__notices.length > 0) {
-        for (var i = 0; i < __notices.length; i++) {
-            var notice = __notices[i];
-            if (notice.name === name) {
-                if (notice.observer === observer) {
-                    return;
-                }
-            }
+  if (__notices.length > 0) {
+    for (var i = 0; i < __notices.length; i++) {
+      var notice = __notices[i]
+      if (notice.name === name) {
+        if (notice.observer === observer) {
+          return
         }
+      }
     }
-	this.addNotification(name, selector, observer)
+  }
+  this.addNotification(name, selector, observer)
 }
 
 function addNotices(newNotice) {
-    // if (__notices.length > 0) {
-    //     for (var i = 0; i < __notices.length; i++) {
-    //         var hisNotice = __notices[i];
-    //         //当名称一样时进行对比，如果不是同一个 则放入数组，否则跳出
-    //         if (newNotice.name === hisNotice.name) {
-    //             if (!cmp(hisNotice, newNotice)) {
-    //                 __notices.push(newNotice);
-    //             }
-    //             return;
-    //         }else{
-    //             __notices.push(newNotice);
-    //         }
+  // if (__notices.length > 0) {
+  //     for (var i = 0; i < __notices.length; i++) {
+  //         var hisNotice = __notices[i];
+  //         //当名称一样时进行对比，如果不是同一个 则放入数组，否则跳出
+  //         if (newNotice.name === hisNotice.name) {
+  //             if (!cmp(hisNotice, newNotice)) {
+  //                 __notices.push(newNotice);
+  //             }
+  //             return;
+  //         }else{
+  //             __notices.push(newNotice);
+  //         }
 
-    //     }
-    // } else {
+  //     }
+  // } else {
 
-    // }
+  // }
 
-    __notices.push(newNotice);
+  __notices.push(newNotice)
 }
 
 /**
@@ -92,18 +93,16 @@ function addNotices(newNotice) {
  * observer: 移除的通知所在的Page对象
  */
 
-function removeNotification(name,observer) {
-    for (var i = 0; i < __notices.length; i++){
-      var notice = __notices[i];
-      if(notice.name === name){
-        if(notice.observer === observer){
-            __notices.splice(i,1);
-            return;
-        }
+function removeNotification(name, observer) {
+  for (var i = 0; i < __notices.length; i++) {
+    var notice = __notices[i]
+    if (notice.name === name) {
+      if (notice.observer === observer) {
+        __notices.splice(i, 1)
+        return
       }
     }
-
-
+  }
 }
 
 /**
@@ -116,76 +115,75 @@ function removeNotification(name,observer) {
  */
 
 function postNotificationName(name, info) {
-    console.log("postNotificationName:" + name);
-    if(__notices.length == 0){
-      console.log("postNotificationName error: u hadn't add any notice.");
-      return;
-    }
+  console.log('postNotificationName:' + name)
+  if (__notices.length == 0) {
+    console.log('postNotificationName error: u hadn\'t add any notice.')
+    return
+  }
 
-    for (var i = 0; i < __notices.length; i++){
-      var notice = __notices[i];
-      if(notice.name === name){
-        notice.selector(info);
-      }
+  for (var i = 0; i < __notices.length; i++) {
+    var notice = __notices[i]
+    if (notice.name === name) {
+      notice.selector(info)
     }
-
+  }
 }
 
 // 用于对比两个对象是否相等
 function cmp(x, y) {
-    // If both x and y are null or undefined and exactly the same
-    if (x === y) {
-        return true;
+  // If both x and y are null or undefined and exactly the same
+  if (x === y) {
+    return true
+  }
+
+  // If they are not strictly equal, they both need to be Objects
+  if (!(x instanceof Object) || !(y instanceof Object)) {
+    return false
+  }
+
+  // They must have the exact same prototype chain, the closest we can do is
+  // test the constructor.
+  if (x.constructor !== y.constructor) {
+    return false
+  }
+
+  for (var p in x) {
+    // Inherited properties were tested using x.constructor === y.constructor
+    if (x.hasOwnProperty(p)) {
+      // Allows comparing x[ p ] and y[ p ] when set to undefined
+      if (!y.hasOwnProperty(p)) {
+        return false
+      }
+
+      // If they have the same strict value or identity then they are equal
+      if (x[p] === y[p]) {
+        continue
+      }
+
+      // Numbers, Strings, Functions, Booleans must be strictly equal
+      if (typeof(x[p]) !== 'object') {
+        return false
+      }
+
+      // Objects and Arrays must be tested recursively
+      if (!Object.equals(x[p], y[p])) {
+        return false
+      }
     }
+  }
 
-    // If they are not strictly equal, they both need to be Objects
-    if (! (x instanceof Object) || !(y instanceof Object)) {
-        return false;
+  for (p in y) {
+    // allows x[ p ] to be set to undefined
+    if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) {
+      return false
     }
-
-    // They must have the exact same prototype chain, the closest we can do is
-    // test the constructor.
-    if (x.constructor !== y.constructor) {
-        return false;
-    }
-
-    for (var p in x) {
-        // Inherited properties were tested using x.constructor === y.constructor
-        if (x.hasOwnProperty(p)) {
-            // Allows comparing x[ p ] and y[ p ] when set to undefined
-            if (!y.hasOwnProperty(p)) {
-                return false;
-            }
-
-            // If they have the same strict value or identity then they are equal
-            if (x[p] === y[p]) {
-                continue;
-            }
-
-            // Numbers, Strings, Functions, Booleans must be strictly equal
-            if (typeof(x[p]) !== "object") {
-                return false;
-            }
-
-            // Objects and Arrays must be tested recursively
-            if (!Object.equals(x[p], y[p])) {
-                return false;
-            }
-        }
-    }
-
-    for (p in y) {
-        // allows x[ p ] to be set to undefined
-        if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) {
-            return false;
-        }
-    }
-    return true;
-};
+  }
+  return true
+}
 
 module.exports = {
-    addNotification: addNotification,
-    removeNotification: removeNotification,
-    postNotificationName: postNotificationName,
-    addOnceNotification: addOnceNotification
+  addNotification: addNotification,
+  removeNotification: removeNotification,
+  postNotificationName: postNotificationName,
+  addOnceNotification: addOnceNotification
 }
