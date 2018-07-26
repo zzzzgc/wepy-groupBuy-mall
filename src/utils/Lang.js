8e80edc833e177ef98a56dab9ca390c472a037f2
zzzzgc <1098766713@qq.com>
2018-07-26 12:16:16 +0800

    工具类目录结构变更

--- src/utils/Lang.js
+++ src/utils/Lang.js
@@ -1,49 +1,54 @@
 export default class Lang {
   // 判断字符串是否为空
   static isEmpty(str) {
-    return str == '' || str == null || str == 'null';
+    return str === '' || str == null || str === 'null'
   }
+
   // 判断字符串是否不为空
   static isNotEmpty(str) {
-    return !this.isEmpty(str);
+    return !this.isEmpty(str)
   }
+
   // 浮点求和
   static sum(numbers, toFixed = 2) {
-    let sum = 0;
+    let sum = 0
     for (const str of numbers) {
       if (!this.isNumber(str)) {
-        return NaN;
+        return NaN
       }
-      const num = parseFloat(str);
+      const num = parseFloat(str)
       if (isNaN(num)) {
-        return NaN;
+        return NaN
       }
-      sum += num;
+      sum += num
     }
-    return sum.toFixed(toFixed);
+    return sum.toFixed(toFixed)
   }
+
   // 数字判断
   static isNumber(value) {
-    const patrn = /^[-+]?\d+(\.\d+)?$/;
-    return patrn.test(value);
+    const patrn = /^[-+]?\d+(\.\d+)?$/
+    return patrn.test(value)
   }
 
   // 数字判断
   static isPositiveNumber(value) {
-    const patrn = /^[1-9]\d*$|^\.\d*$|^0\.\d*$|^[1-9]\d*\.\d*$|^0$/;
-    return patrn.test(value);
+    const patrn = /^[1-9]\d*$|^\.\d*$|^0\.\d*$|^[1-9]\d*\.\d*$|^0$/
+    return patrn.test(value)
   }
+
   // 数组判断
   static isArray(o) {
-    return Object.prototype.toString.call(o) === '[object Array]';
+    return Object.prototype.toString.call(o) === '[object Array]'
   }
+
   // 事件转日期
   static convertTimestapeToDay(timestape) {
-    return timestape.substring(0, timestape.indexOf(' ')).replace(/-/g, '.');
+    return timestape.substring(0, timestape.indexOf(' ')).replace(/-/g, '.')
   }
 
   // 格式化日期
-  static dateFormate (date, fmt) {
+  static dateFormate(date, fmt) {
     const o = {
       'M+': date.getMonth() + 1,
       'd+': date.getDate(),
@@ -52,11 +57,25 @@ export default class Lang {
       's+': date.getSeconds(),
       'q+': Math.floor((date.getMonth() + 3) / 3),
       'S': date.getMilliseconds()
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
-    for (let k in o) {
-      if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
     }
-    return fmt;
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+    for (let k in o) {
+      if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+    }
+    return fmt
+  }
+
+  /**
+   * 使用循环的方式判断一个元素是否存在于一个数组中
+   * @param {Object} arr 数组
+   * @param {Object} value 元素值
+   */
+  static arrIndexOf(arr, value) {
+    for (var i = 0; i < arr.length; i++) {
+      if (value === arr[i]) {
+        return true
+      }
+    }
+    return false
   }
 }
