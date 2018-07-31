b8b50225851f8ff27305602ca7843541a08feca5
zzzzgc <1098766713@qq.com>
2018-07-31 18:56:12 +0800

    工具类目录结构变更

---
+++ src/utils/user/UserInfo.js
@@ -0,0 +1,31 @@
+// 获取用户自提点地址
+export default class UserInfo {
+getNoutoasiakasAddress(){
+  if (this.$parent.globalData.noutoasiakasAddress != null) {
+    this.noutoasiakasAddress = this.$parent.globalData.noutoasiakasAddress
+  } else {
+    // TODO 加载用户默认自提点配置
+    let noutoasiakasAddressInfo = [
+      {
+        address: '北京路某某街某某大道1号街',
+        name: '自提点1',
+        addressId: 13,
+        checked: true
+      },
+      {
+        address: '北京路某某街某某大道2号街',
+        name: '自提点2',
+        addressId: 14,
+        checked: true
+      },
+      {
+        address: '北京路某某街某某大道3号街',
+        name: '自提点3',
+        addressId: 15,
+        checked: true
+      }
+    ]
+    this.noutoasiakasAddress = noutoasiakasAddressInfo
+    this.$parent.globalData.noutoasiakasAddress = noutoasiakasAddressInfo
+  }
+}
